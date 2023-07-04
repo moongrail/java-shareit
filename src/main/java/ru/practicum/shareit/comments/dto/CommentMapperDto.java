24f7d058363eb2677dda2efc8d5b93934a5305e2
Moongrail <omgone666@yandex.ru>
2023-07-04 05:58:10 +0300

    fix: time fix

--- src/main/java/ru/practicum/shareit/comments/dto/CommentMapperDto.java
+++ src/main/java/ru/practicum/shareit/comments/dto/CommentMapperDto.java
@@ -6,15 +6,23 @@ import ru.practicum.shareit.item.model.Item;
 import ru.practicum.shareit.user.model.User;
 
 import java.time.LocalDateTime;
+import java.time.OffsetDateTime;
+import java.time.ZoneId;
+import java.time.ZoneOffset;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @UtilityClass
 public class CommentMapperDto {
+    private static final ZoneId ZONE_ID = ZoneId.systemDefault();
+    private static final ZoneOffset ZONE_OFFSET = OffsetDateTime.now().getOffset();
+
     public static CommentResponseDto toCommentResponseDto(Comment comment) {
+        LocalDateTime created = LocalDateTime.ofInstant(comment.getCreated().toInstant(ZONE_OFFSET), ZONE_ID);
+
         return CommentResponseDto.builder()
                 .id(comment.getId())
-                .created(comment.getCreated())
+                .created(created)
                 .text(comment.getText())
                 .authorName(comment.getAuthor().getName())
                 .build();
